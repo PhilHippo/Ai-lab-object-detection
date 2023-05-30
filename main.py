e3d19ab8dd5a00e9ad42ab6e131a9efb1db06d03
SimonLebron <Simo02ru@gmail.com>
2023-05-30 22:04:32 +0200

    codice finito

--- main.py
+++ main.py
@@ -2,24 +2,24 @@ import cv2 as cv
 from utility import *
 
 # load the trained model
-cascade_model = cv.CascadeClassifier('cascade\haarcascade_eye_tree_eyeglasses.xml')
+cascade_model = cv.CascadeClassifier('cascadebest/cascade.xml')
 cap = cv.VideoCapture(0)
 
-    #list of audio paths
-    #Windows traditionally uses the backslash (\) to separate directories in file paths
-    #other operationg systems (including Mac OS X and Linux) use forward slash (/)
-    audio_paths = {"up": 'audio\\up.mp3', "down": 'audio\\down.mp3', "left": 'audio\\left.mp3', "right": 'audio\\right.mp3', "shoot": 'audio\\shoot.mp3'}
-    audio_paths = {"up": 'audio/up.mp3', "down": 'audio/down.mp3', "left": 'audio/left.mp3', "right": 'audio/right.mp3', "shoot": 'audio/shoot.mp3'}
+#list of audio paths
+#Windows traditionally uses the backslash (\) to separate directories in file paths
+#other operationg systems (including Mac OS X and Linux) use forward slash (/)
+audio_paths = {"up": 'audio\\up.mp3', "down": 'audio\\down.mp3', "left": 'audio\\left.mp3', "right": 'audio\\right.mp3', "shoot": 'audio\\shoot.mp3'}
+audio_paths = {"up": 'audio/up.mp3', "down": 'audio/down.mp3', "left": 'audio/left.mp3', "right": 'audio/right.mp3', "shoot": 'audio/shoot.mp3'}
 
-    # get the frame size and center
-    frame_size = get_frame_size(cap)
-    frame_center = get_rectangle_center((0, 0, frame_size[0], frame_size[1]))
+# get the frame size and center
+frame_size = get_frame_size(cap)
+frame_center = get_rectangle_center((0, 0, frame_size[0], frame_size[1]))
 
-    frame_counter= 0
+frame_counter = 0
 
-    while(True):
-        # get an updated image of the webcam
-        ret, frame = cap.read()
+while(True):
+    # get an updated image of the webcam
+    frame = cap.read()[1]
 
     #pipeline filters
     adjusted_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)   
@@ -37,14 +37,14 @@ cap = cv.VideoCapture(0)
         speak(rectangles, frame_center, audio_paths)
         frame_counter = 0
 
-        cv.imshow('Matches', detection_image)
+    cv.imshow('Matches', detection_image)
 
-        frame_counter += 1
+    frame_counter += 1
 
-        # press 'q' with the output window focused to exit.
-        key = cv.waitKey(1)
-        if key == ord('q'):
-            cv.destroyAllWindows()
-            break
-        
-    print('Done.')
+    # press 'q' with the output window focused to exit.
+    key = cv.waitKey(1)
+    if key == ord('q'):
+        cv.destroyAllWindows()
+        break
+
+print('Done.')
